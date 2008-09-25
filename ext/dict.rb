68bbbff0b121b216fc856b3f743402ad11bfedc3
Aurelian Oancea <aurelian@locknet.ro>
2008-09-25 22:18:54 +0200

    * fix the dict file path.

--- ext/dict.rb
+++ ext/dict.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
+
 require 'ahocorasick.bundle'
 
-k= AhoCorasick::KeywordTree.from_file "../en.all"
+k= AhoCorasick::KeywordTree.from_file "../__tbr/en.all"
 
 #puts k.size
 #k.add_string("foo4");
